3f62a7225c2d10a1a14784650155fd9380a2a004
Nemessis13 <nemessisdesign@gmail.com>
2022-02-23 11:47:49 -0300

    fix rutas

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-let dado1 = ["Dado1-ellas.svg",
-    "Dado1-ellos.svg",
-    "Dado1-nadie.svg",
-    "Dado1-reto.svg",
-    "Dado1-todos.svg",
-    "Dado1-tu.svg",
+let dado1 = ["./img/Dado1-ellas.svg",
+    "./img/Dado1-ellos.svg",
+    "./img/Dado1-nadie.svg",
+    "./img/Dado1-reto.svg",
+    "./img/Dado1-todos.svg",
+    "./img/Dado1-tu.svg",
  ];
 
-let dado2 = ["Dado2-1trago.svg",
-    "Dado2-2tragos.svg",
-    "Dado2-bailar.svg",
-    "Dado2-cantar.svg",
-    "Dado2-mediotrago.svg",
-    "Dado2-NoseToma.svg",
+let dado2 = ["./img/Dado2-1trago.svg",
+    "./img/Dado2-2tragos.svg",
+    "./img/Dado2-bailar.svg",
+    "./img/Dado2-cantar.svg",
+    "./img/Dado2-mediotrago.svg",
+    "./img/Dado2-NoseToma.svg",
  ];
 
 
